fe4554677569193d60d438a7b13b2858450d9dd4
shiro <shiro@usagi.io>
2025-11-27 12:14:15 +0900

    Add shell nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let 
+    rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+    pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+    rust = pkgs.rust-bin.stable."1.91.1".default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "wasm32-unknown-unknown" ];
+    };
+in
+pkgs.mkShell { 
+    buildInputs = [
+      rust
+    ] ++ (with pkgs; [
+    ]);
+    nativeBuildInputs = with pkgs; [];
+    packages = with pkgs; [
+      wasm-pack
+    ];
+
+    RUST_BACKTRACE = 1;
+}
