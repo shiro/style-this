8a46125e67468f49d93e62179540ea89b9fcd955
shiro <shiro@usagi.io>
2026-08-01 12:42:11 +0900

    Use vitest test

---
+++ vitest.workspace.ts
@@ -0,0 +1,9 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  'packages/core',
+  'packages/next',
+  'packages/solid',
+  'packages/vite',
+  'packages/webpack',
+])
